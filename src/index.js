c4399cb0e8edbfae4aa872c9c161504ec12459ca
chenjiezi <892774940@qq.com>
2022-06-06 15:05:33 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Author: chenjz
+ * @Date: 2022-06-06 14:11:11
+ * @LastEditors: chenjz
+ * @LastEditTime: 2022-06-06 14:32:57
+ */
+
+import './assets/css/index.less'
+
+const test = () => {
+  console.log('test webpack')
+}
+
+test()
---
+++ webpack.config.js
@@ -0,0 +1,135 @@
+/*
+ * @Description: 
+ * @Author: chenjz
+ * @Date: 2022-06-06 11:58:30
+ * @LastEditors: chenjz
+ * @LastEditTime: 2022-06-06 15:02:50
+ */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+    contentBase: '../dist'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin()
+    ]
+  },
+  module: {
+    rules: [
+      // babel
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      // css
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      // less
+      {
+        test: /.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      // 图片
+      {
+        test: /\.(jpg|png|jpeg|gif|bmp)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1024,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]'
+              }
+            }
+          }
+        }
+      },
+      // 压缩图片
+      {
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            progressive: true,
+          },
+          optipng: {
+            enabled: false,
+          },
+          pngquant: {
+            quality: [0.65, 0.90],
+            speed: 4
+          },
+          gifsicle: {
+            interlaced: false,
+          },
+          webp: {
+            quality: 75
+          }
+        }
+      }
+      // 音频
+      {
+        test: /\.(mp4|ogg|mp3|wav)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1024,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]'
+              }
+            }
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      title: 'Hello Webpack',
+      minify: {
+        collapseWhitespace: true, // 去掉空格
+        removeComments: true // 去掉注释
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new OptimizeCssAssetsWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'
+    })
+  ]
+}
